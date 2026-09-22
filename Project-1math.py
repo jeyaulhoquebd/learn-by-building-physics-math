55fb89e00267570f97ad0f8d23af62a60bfc05a6
jmathcodebd <jmathcode@gmail.com>
2026-09-22 19:38:20 +0600

    first commit

---
+++ Project-1math.py
@@ -0,0 +1,9 @@
+"""
+Project 1: Projectile Motion Simulator
+বিষয়: Physics + Math | লেভেল: Basic Python
+ 
+এই স্ক্রিপ্টটা ৫টা ধাপে ভাগ করা। প্রতিটা ধাপ একটা function।
+নিচ থেকে উপরে না পড়ে, উপর থেকে নিচে পড়ুন - main() function-টাই
+সব ধাপকে একসাথে জোড়া দেয়।
+"""
+
